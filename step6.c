b7aace60ea66d91a71e89b17af815555fb8a73bb
jfelic <felicianojulian1@gmail.com>
2025-01-09 16:28:47 -0500

    Complete Lesson 2 Step 6

---
+++ step6.c
@@ -0,0 +1,19 @@
+/*
+    Write a program that reads lines from the console and prints them out as they are read. 
+    The program should stop reading when it reads an empty (blank) line.
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[256];
+
+    printf("Enter lines of text. Enter a blank line to stop.\n");
+
+    do {
+        fgets(str, 255, stdin); 
+        printf("You entered: %s", str);
+    } while (str[0] != '\n');
+
+    return 0;
+}
