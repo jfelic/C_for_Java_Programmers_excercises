3e98b12bde6491167dd1f24eee25613a827a4044
jfelic <felicianojulian1@gmail.com>
2025-01-09 11:19:02 -0500

    Initial commit

---
+++ f_to_k.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*/ 
+	Write a program that stores a temperature in degrees Fahrenheit using a variable and 
+	converts it to Kelvins using the following formula: K = (((F - 32) * 5) / 9) + 273.15
+*/ 
+float f_to_k(int f) {
+	float k = (((f - 32) * 5) / 9.0) + 273.15;
+	return k;
+}
+
+int main() {
+	float f = 32;
+	float k = f_to_k(f);
+	printf("%.2fF = %.2fK\n", f, k);
+	return 0;
+}
