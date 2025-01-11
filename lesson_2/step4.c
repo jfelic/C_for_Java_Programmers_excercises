f292a0ce199484a236777e5d3f0821d9e5506887
jfelic <felicianojulian1@gmail.com>
2025-01-11 15:24:34 -0500

    Began working on step 7 of lesson 2

---
+++ lesson_2/step4.c
@@ -0,0 +1,34 @@
+/*
+    C is not an object-oriented language, so it doesn’t have methods defined in classes. 
+    Instead, it has the more general idea of a “function”. 
+    This is covered in the C for Java Programmers document. 
+    Object-oriented programming in C is covered in the next lesson in this tutorial.
+
+    For now, write the following two functions:
+
+        A function that has a single integer parameter and that prints that many stars (asterisks).
+        A function that computes and returns the temperature in Kelvin of a temperature given in degrees Fahrenheit.
+
+    Include code to demonstrate your functions being used. 
+*/
+#include <stdio.h>
+
+void stars(int num) {
+    for (int i = 0; i < num; i++) {
+        printf(" *");
+    }
+}
+
+float f_to_k(int f) {
+	float k = (((f - 32) * 5) / 9.0) + 273.15;
+	return k;
+}
+
+int main() {
+    printf("Stars:");
+    stars(10);
+    printf("\n");
+    
+    printf("58 F = %.2f K\n", f_to_k(58));
+    return 0;
+}
---
+++ lesson_2/step7.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+    The function fprintf is just like printf, except that instead of printing to the console (“standard output”), 
+    it prints to the output FILE* given as first argument.
+
+    The function fscanf is just like scanf, except that instead of reading from the console (“standard inpout”), 
+    it reads from the input FILE* given as first argument.
+*/
+
+/*
+    write a function that takes two filenames as parameters and copies the contents of the file with the first name
+    into a file with the second name (creating it if it doesn’t exist, overwriting it if it does exist, both of which 
+    are the default behavior of fopen). 
+*/
+
+int copy_file(char* srcfilename, char* dstfilename) {
+    // First we open the src file and enable reading
+    FILE* src = fopen(srcfilename, "r");
+    if (src == NULL) {
+        printf("Error opening src file");
+        return 0;
+    }
+
+    // Then we open destination file and enable writing
+    FILE* dst = fopen(dstfilename, "w");
+    if (dst == NULL) {
+        printf("Error opening destination file");
+        return 0;
+    }
+
+    while(!feof(src)) {
+        char str[256];
+        if (fgets(str, 255, src) != NULL) {
+            fprintf(dst, "%s", str);
+        }
+    }
+
+    fclose(src);
+    fclose(dst);
+    return 1; // Return 1 meaning success
+}
+
+int main() {
+    
+}
