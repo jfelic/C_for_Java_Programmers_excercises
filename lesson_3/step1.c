b10f642a77829d1b1e4719c1b4e563bb6704bd83
jfelic <felicianojulian1@gmail.com>
2025-01-16 11:16:45 -0500

    Working through examples in Ch8: Memory Management in C

--- lesson_3/step1.c
+++ lesson_3/step1.c
@@ -6,6 +6,15 @@ struct Employee {
     int id;
 };
 
+void time2hms(int time, int* hp, int* mp, int* sp)
+{
+    *hp = time / (60 * 60);
+    time -= *hp * 60 * 60;
+    *mp = time / 60;
+    time -= *mp * 60;
+    *sp = time;
+}
+
 // "Constructor" function to create a new Employee struct
 struct Employee* new_employee(char* name, int id) {
     struct Employee* new_employee = (struct Employee*)malloc(sizeof(struct Employee));
@@ -22,6 +31,17 @@ int main() {
     int* i_p = &i; // Initialized to the address in memory of the variable i
 
     printf("%d\n", *i_p); // Dereference i_p in order to get the value it is pointing at
+
+    *i_p = 987; // Change value of int i by rereferencing it's pointer
+    printf("%d, %d\n", i, *i_p);
+
+    int time = 15623;
+    printf("--- %d seconds to hours, minutes, and seconds ---\n", time);
+    int hours, minutes, seconds;
+
+    time2hms(time, &hours, &minutes, &seconds);
+
+    printf("Hours: %d, Minutes: %d, Seconds: %d\n", hours, minutes, seconds);
 
     return 0;
 }
