22927e4ce7a5807ffc9c1bd6b5ee3a14eeaad060
jfelic <felicianojulian1@gmail.com>
2025-01-16 22:29:01 -0500

    Lesson 8 sections 1, 2, 3, 4, and 5 completed.

--- lesson_3/step1.c
+++ lesson_3/step1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Employee {
+struct Employee 
+{
     char* name;
     int id;
 };
@@ -19,14 +20,15 @@ void time2hms(int time, int* hp, int* mp, int* sp)
 struct Employee* new_employee(char* name, int id) {
     struct Employee* new_employee = (struct Employee*)malloc(sizeof(struct Employee));
     if (new_employee == NULL) {
-        return NULL; // Out of memory
+        return NULL; // Allocation failed
     }
     new_employee->name = name;
     new_employee->id = id;
     return new_employee;
 }
 
-int main() {
+int main() 
+{
     int i = 123; // Stores the value 123 somewhere in memory
     int* i_p = &i; // Initialized to the address in memory of the variable i
 
@@ -35,6 +37,7 @@ int main() {
     *i_p = 987; // Change value of int i by rereferencing it's pointer
     printf("%d, %d\n", i, *i_p);
 
+    // Beginning of time2hms practice:
     int time = 15623;
     printf("--- %d seconds to hours, minutes, and seconds ---\n", time);
     int hours, minutes, seconds;
@@ -43,5 +46,18 @@ int main() {
 
     printf("Hours: %d, Minutes: %d, Seconds: %d\n", hours, minutes, seconds);
 
+    // Practice malloc()
+    int* ip = (int*)malloc(sizeof(int));
+    if (ip == NULL) 
+    {
+        // Allocation failed
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    *ip = 98765;
+    printf("Malloc int pointer: %d\n", *ip);
+    free(ip);
+    ip = NULL;
+    
     return 0;
 }
